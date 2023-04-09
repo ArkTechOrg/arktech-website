c8f2907d376669289369ec42fb59af7a073ca8cc
captain-n3m0 <techtubebydebjit@gmail.com>
2023-04-09 21:33:32 +0530

    minor changes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import linuxLogo from './assets/linuxgang-penguin.svg'
+import { useState } from 'react';
+import './App.css';
+import linuxLogo from './assets/linuxgang-penguin.svg';
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      {/*
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <header>
+        <h1>Welcome to our Open Source Development Site</h1>
+        <p>Explore our projects, contribute to the community, and learn from others.</p>
+      </header>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -27,15 +19,15 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      */}
-      <div>
-        <img align="right" src={linuxLogo} className="logo" />
+      <footer>
+        <p>License: MIT</p>
+        <p>Contribute: <a href="https://github.com/Linux-Gang">https://github.com/Linux-Gang</a></p>
+      </footer>
+      <div className="penguin-container">
+      <img align="right" src={linuxLogo} className="penguin-container" />
       </div>
     </div>
   )
 }
 
-export default App
+export default App;
