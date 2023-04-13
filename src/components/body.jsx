dcea9a89e36764abfadf357be58bf71094dcf5bf
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-04-13 22:07:52 +0530

    fix: changed alignment of join link buttons

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -8,8 +8,10 @@ export default function Body() {
             <section>
                 <h1>Join the GNU/Linux Gang</h1>
                 <p>Collaborate with Developers and work on Open Source Projects</p>
-                {isClicked ? <><button className="get__started__button">WhatSapp</button><button className="get__started__button">Discord</button></>
-                    : <button className="get__started__button" onClick={() => setClicked(true)}>Get Started</button>}
+                <div className="get__started__button__container">
+                    {isClicked ? <><button className="get__started__button">WhatSapp</button><button className="get__started__button">Discord</button></>
+                        : <button className="get__started__button" onClick={() => setClicked(true)}>Get Started</button>}
+                </div>
             </section>
             <div class="penguin">
                 <img src={Penguin} alt="Linux Gang Penguin" />
