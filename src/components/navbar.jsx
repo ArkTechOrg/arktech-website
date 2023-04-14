28d0db46657f9347ff59e0f13637c0200efd16b6
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-04-14 19:53:37 +0530

    feat: made the GitHub button clickable

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -57,6 +57,7 @@ function Navbar() {
             width="30px"
             src={githubMarkWhite}
             alt="GitHub Link"
+            onClick={() => window.open("https://github.com/Linux-Gang")}
           />
         </div>
       </motion.nav>
